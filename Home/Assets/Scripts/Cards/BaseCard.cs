3ebf6d864848ffda3ecee6ea6d2a74a03a01dd8c
Memnun <zeronocool@gmail.com>
2019-01-26 07:46:58 -0500

    fixed an oopsie-doodle 7:46-saturday

--- Home/Assets/Scripts/Cards/BaseCard.cs
+++ Home/Assets/Scripts/Cards/BaseCard.cs
@@ -11,15 +11,18 @@ public class BaseCard : MonoBehaviour
     //the function to call when a player uses this particular card; target single player
     public bool Activation (Player target) {
     	//
+        return false;
     }
 
     //function for using the card with two player targets
     public bool Activation (Player target1, Player target2) {
     	//
+        return false;
     }
 
     //more abstract 2-target activator, for modifying non-player data
     public bool Activation (GameObject target1, GameObject target2) {
     	//
+        return false;
     }
 }
--- Home/Assets/Scripts/TileEvents/BaseTileEvent.cs
+++ Home/Assets/Scripts/TileEvents/BaseTileEvent.cs
@@ -9,15 +9,18 @@ public class BaseTileEvent : MonoBehaviour
     //the function to call when a player triggers this event; target single player
     public bool Activation (Player target) {
     	//
+        return false;
     }
 
     //function for triggering the event with two player targets
     public bool Activation (Player target1, Player target2) {
     	//
+        return false;
     }
 
     //more abstract 2-target activator, for modifying non-player data
     public bool Activation (GameObject target1, GameObject target2) {
     	//
+        return false;
     }
 }
