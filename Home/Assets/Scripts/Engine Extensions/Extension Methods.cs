879db21fb986d9f7c747b239db8c0e24dedfd250
Memnun <zeronocool@gmail.com>
2019-04-18 14:53:13 -0400

    added more tools to unity
    to make our future lives easier

---
+++ Home/Assets/Scripts/Engine Extensions/Extension Methods.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ExtensionMethods {
+	
+	public static float Map (float inputValue, float inputLow, float inputHigh, float outputLow, float outputHigh) {
+		return (inputValue - inputLow) / (inputHigh - inputLow) * (outputHigh - outputLow) + outputLow;
+	}
+	
+}
